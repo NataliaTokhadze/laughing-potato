1c01261a87488c4b25fca483054ac77c152d8ae7
Natalia <147244357+NataliaTokhadze@users.noreply.github.com>
2025-04-06 14:23:24 +0400

    Add files via upload

--- chad.store/config/settings.py
+++ chad.store/config/settings.py
@@ -156,3 +156,16 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'natalia.tokhadze@gmail.com'
+EMAIL_HOST_PASSWORD = "mjss ghfh lbhe tpri"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
